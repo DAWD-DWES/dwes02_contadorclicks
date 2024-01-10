8868d13946604dc7aa508b20e9136917444a8274
Iván <jivanrod@gmail.com>
2024-01-10 19:08:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-if (!empty($_POST)) {
-    $numClicks = $_POST['numclicks'];
-    $numClicks++;
+session_start();
+if (empty($_POST)) {
+    $numClicks = $_SESSION['numClicks'] = 0;
+}
+else {
+$numClicks = ++$_SESSION['numClicks'];
 }
 ?>
 <!DOCTYPE html>
@@ -12,8 +15,7 @@ if (!empty($_POST)) {
     </head>
     <body>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
-            <h1>El número de clicks hasta ahora es: <?= $numClicks ?? 0 ?></h1>
-            <input type="hidden" id="id" name="numclicks" value="<?= $numClicks ?? 0 ?>"> <!-- Valor oculto incrustado en el formulario -->
+            <h1>El número de clicks hasta ahora es: <?= $numClicks ?></h1>
             <input type="submit" value="Click" name="click">  
         </form>
     </body>
